429df3e12db3c181a69c2a14f2b67af8419ec221
Fernanda Brito <nanda.britto@gmail.com>
2022-04-24 12:54:18 +0100

    Removed some code

--- seleniumcourse.py
+++ seleniumcourse.py
@@ -1,23 +1,5 @@
-import math
-import os
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Fernanda'))
 
 r = requests.get('http://coreyms.com')
 print(r.status_code)
-
-# name = input('Your name? ')
-# print('Hello,', name)
+print(r.ok)
